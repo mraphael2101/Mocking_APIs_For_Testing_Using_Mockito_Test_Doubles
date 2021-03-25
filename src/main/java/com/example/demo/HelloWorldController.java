505f11edabcee340e6118359ee95c72040dea363
mraphael2101 <mark.raphael@ymail.com>
2021-03-25 19:28:12 +0000

    initial commit

---
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/greetings/{name}")
+    public @ResponseBody Message greeting(@PathVariable String name) {
+        return new Message(name);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class Message {
+
+    private String message = "Hello World";
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
